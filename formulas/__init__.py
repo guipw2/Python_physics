2d71d36e719088a2fd1637759dadc44b5e646c71
Guilherme Dreyer <guilhermedreyer.contato@gmail.comgit config --global user.email guilhermedreyer.contato@gmail.com>
2021-06-28 22:10:36 -0300

    optimization of options functions and other minor fixes

--- formulas/__init__.py
+++ formulas/__init__.py
@@ -1,6 +1,6 @@
 def fisica():
     def title(ttl='Title Not Found', tpl=1):
-        print('-' * 10, f'{ttl}', '-' * 10)
+        print('\n-' * 10, f'{ttl}', '-' * 10)
 
     def linha(op, sz=20):
         if op == 1:
@@ -13,58 +13,34 @@ def fisica():
             print('=-' * sz)
 
     def opcoes():
-        sleep(1)
-        linha(4)
-        print('1)Cinemática')
-        sleep(0.5)
-        linha(2)
-        sleep(0.1)
-        print('2)Dinâmica')
-        sleep(0.5)
-        linha(2)
-        sleep(0.1)
-        print('3)Trabalho, potência e energia')
-        sleep(0.5)
-        linha(2)
-        sleep(0.1)
-        print('4)Impulso e Quantidade de Movimento')
-        sleep(0.5)
-        linha(2)
-        sleep(0.1)
-        print('5)Hidrostática')
-        sleep(0.5)
-        linha(2)
-        sleep(0.1)
-        print('6)Gravitação Universal')
-        sleep(0.5)
-        linha(2)
-        sleep(0.1)
-        print('7)Termologia e Termodinâmica')
-        sleep(0.5)
-        linha(2)
-        sleep(0.1)
-        print('8)Ondas e Ótica')
-        sleep(0.5)
-        linha(2)
-        sleep(0.1)
-        print('9)Eletricidade')
-        sleep(0.5)
-        linha(2)
-        sleep(0.1)
-        print('10)Eletromagnetismo')
-        linha(4)
+        options = ('Cinemática', 'Dinâmica', 'Trabalho, potência e energia',
+                   'Impulso e Quantidade de Movimento', 'Hidrostática', 'Gravitação Universal',
+                   'Termologia e Termodinâmica', 'Ondas e Ótica', 'Eletricidade', 'Eletromagnetismo')
+        n = 0
+        for item in options:
+            n += 1
+            if n == 1:
+                linha(4)
+            else:
+                linha(2)
+            print(f'{n}){item}')
+            if n == len(options):
+                linha(4)
 
     def cinamatica():
         def opcoescnm():
-            linha(4)
-            print('1)Movimento Retilíneo Uniforme')
-            linha(2)
-            print('2)Movimento Retilíneo Uniformemente Variado')
-            linha(2)
-            print('3)Movimento Circular Uniforme')
-            linha(2)
-            print('4)Lançamento Oblíquo')
-            linha(4)
+            options = ('Movimento Retilíneo Uniforme', 'Movimento Retilíneo Uniformemente Variado',
+                       'Movimento Circular Uniforme', 'Lançamento Oblíquo')
+            n = 0
+            for item in options:
+                n += 1
+                if n == 1:
+                    linha(4)
+                else:
+                    linha(2)
+                print(f'{n}){item}')
+                if n == len(options):
+                    linha(4)
 
         while True:
             title('Cinemática')
@@ -73,10 +49,10 @@ def fisica():
             resp = str(input('Sua escolha: '))
             linha(4)
             if resp == '1':
-                s0 = float(input('s0= '))
-                v = float(input('v='))
-                t = float(input('∆t='))
-                print(f's={s0 + v * t}')
+                s0 = float(input('s0(posição inicial)= '))
+                v = float(input('v(velocidade)='))
+                t = float(input('∆t(intervalo de tempo)='))
+                print(f's(posição final)={s0 + v * t}')
             elif resp == '<':
                 break
 
@@ -85,7 +61,7 @@ def fisica():
     sleep(1)
 
     while True:
-        print('Escolha a opção digitando o numero respectivo dela:')
+        print('Escolha a opção digitando o respectivo número dela:')
         opcoes()
         resp = str(input('Sua escolha:'))
         linha(4)
